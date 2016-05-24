352961430176c7ae4f230e29359c885c0d2f5d40
Pat Marshall <pmarshall1716@gmail.com>
2016-05-24 13:59:48 -0600

    edited entries

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,42 @@
+class EntriesController < ApplicationController
+  def index
+  	@entries = Entry.all
+  end
+
+  def show
+  	@entry = Entry.find(params[:id])
+  end
+
+  def new
+  	@entry = Entry.new
+  end
+
+  def create
+  	@entry = Entry.new(entry_params)
+
+  	if @entry.save
+  		redirect_to entries_path
+  	else
+  		render :new
+  	end
+  end
+
+  def edit
+    @entry = Entry.find(params[:id])
+  end
+
+  def update
+    @entry = Entry.find(params[:id])
+      if @entry.update(entry_params)
+        redirect_to entries_path
+      else
+        render :edit
+      end
+  end
+
+  private 
+
+  def entry_params
+  	params.require(:entry).permit(:title, :body)
+  end
+end
